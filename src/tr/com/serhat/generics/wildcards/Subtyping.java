4bb26173e286b7b3c15f6b91b58c6870f8fd9052
unknown <serhatozkan@hotmail.com>
2023-02-15 22:23:43 +0300

    Altered comment

--- src/tr/com/serhat/generics/wildcards/Subtyping.java
+++ src/tr/com/serhat/generics/wildcards/Subtyping.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class Subtyping {
 
     public void demo() {
-
         List<Integer> integerList = new ArrayList<>();
         List<? extends Integer> extendsIntList = integerList;
         List<? extends Number> extendsNumList = extendsIntList;
         List<?> objList = extendsNumList;
 
-
+        // List<? extends Integer> is child of List<? extends Number>
+        // If B extends A
+        // Class<? extends B> is child of Class<? extends A>
     }
 }
